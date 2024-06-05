87225924f3a02e254b7a96f03cb667d1517384ae
Manuela Barbic <manuelabeatrice.barbic@edu.fh-kaernten.ac.at>
2024-06-05 13:14:43 +0200

    some files were untracked?

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Header } from "./components/Header";
+import { MovieSearch } from "./components/MovieSearch";
+
+function App() {
+  return (
+    <>
+      <Header page="home" />
+      <MovieSearch />
+    </>
+  );
+}
+
+export default App;
---
+++ src/types/type.ts
@@ -0,0 +1,7 @@
+export type favoriteMovieType = {
+  id: number;
+  imdbID: string;
+  isFavorite: boolean;
+  starRating: number;
+  comments: string[];
+};
